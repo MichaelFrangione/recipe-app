7c3c3b7ad2b8a81e2fa6220550161368749f4eef
Michael Frangion <michael.frangione@digiflare.com>
2017-04-08 17:58:08 -0400

    Changed many things.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import {
 } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Login from './components/Login/Login';
+import { Jumbotron, Button, Grid, Row, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
 
-import logo from './logo.svg';
+
 import './main.css';
 
 class App extends Component {
@@ -21,13 +22,27 @@ class App extends Component {
               <Route path="/login" component={Login}/>
             </div>
           </Router>
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Recipe App</h2>
+
+          <div className="container home">
+            <Row className="show-grid">
+              <Col xs={12} md={8}>
+                <Jumbotron className="home__jumbotron">
+                  <h2>Welcome to Recipe App!</h2>
+                  <p>This app will hopefully do something some day so for now I'm just filling up space It also probably won't show when you are logged in...</p>
+                  <p>Login or Register to Get Started!</p>
+                  <p><Button bsStyle="primary">Take me there</Button></p>
+                </Jumbotron>
+              </Col>
+              <Col xs={12} md={4}>
+                <h2>Random Recipes</h2>
+                <ListGroup>
+                  <ListGroupItem href="#link1">Hiyashi Beef</ListGroupItem>
+                  <ListGroupItem href="#link2">Pesto Pasta</ListGroupItem>
+                  <ListGroupItem href="#link2">Okonomiyaki</ListGroupItem >
+                </ListGroup>
+              </Col>
+              </Row>
           </div>
-          <p className="App-intro">
-            Login to Get Started!
-          </p>
         </div>
     );
   }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,18 +4,61 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+
+import logo from '../../images/logo.png';
 
 export default class Header extends Component {
+
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+
+    }
+
     render() {
         return (
             <header className="header">
-                <ul className="header__nav">
-                    <li className="header__nav__list-item">
-                        <Link to="login">Login</Link>
-                    </li>
-                    <li className="header__nav__list-item">Register</li>
-                </ul>
+                <Navbar inverse collapseOnSelect className="header__nav">
+                    <Navbar.Header>
+                    <Navbar.Brand className="header__nav__brand">
+                        <img src={logo} className="App-logo pull-left" alt="logo" />
+                        <a href="#" className="pull-left">Recipe App</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">Featured Recipes</NavItem>
+                        <NavItem eventKey={2} href="#">Random Recipe</NavItem>
+                    </Nav>
+
+                        
+                    { this.props.isLoggedIn ? (
+                        <Nav pullRight>
+                            <NavDropdown eventKey={3} title="Username" id="basic-nav-dropdown">
+                                <MenuItem eventKey={3.1}>Account</MenuItem>
+                                <MenuItem eventKey={3.2}>My Recipes</MenuItem>
+                                <MenuItem eventKey={3.3}>Settings</MenuItem>
+                                <MenuItem divider />
+                                <MenuItem eventKey={3.4}>Logout</MenuItem>
+                            </NavDropdown>
+                        </Nav>) : ( 
+
+                        <Nav pullRight>
+                            <NavItem eventKey={1} href="#">Login</NavItem>
+                            <NavItem eventKey={2} href="#">Register</NavItem>
+                        </Nav>)
+                    }
+                    </Navbar.Collapse>
+                </Navbar>
             </header>
         )
     }
 };
+
+Header.defaultProps = {
+    isLoggedIn: false
+}
