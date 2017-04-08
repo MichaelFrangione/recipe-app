bf1869e6a478a2c5fc60bcac123d5f1b0661ddb5
Michael Frangion <michael.frangione@digiflare.com>
2017-04-08 19:58:21 -0400

    Added home page layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Login from './components/Login/Login';
-import { Jumbotron, Button, Grid, Row, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Jumbotron, Button, Grid, Row, Col, Table, Thumbnail, Glyphicon } from 'react-bootstrap';
 
+import samplePublicRecipes from "./sample_json.js";
 
 import './main.css';
 
 class App extends Component {
-  
+
+  getPublicRecipes() {
+    let recipes = samplePublicRecipes.recipes;
+     
+     return (
+        recipes.map((recipe, i) => {
+            
+            if (i > 2) return;
+
+            return ( 
+                    <tr key={recipe.id}>
+                      <td><img src={recipe.images['thumbnail']} height="50" width="50"/></td>
+                      <td>{recipe.name}</td>
+                      <td>{recipe.author}</td>
+                    </tr>
+            )
+        })
+     )
+  }
+
+   getFeaturedPublicRecipes() {
+    let recipes = samplePublicRecipes.recipes;
+     
+     return (
+        recipes.map((recipe) => {
+            return (
+                    <Col xs={6} md={3} key={recipe.id}>
+                      <Thumbnail src={recipe.images['thumbnail']} alt="242x200">
+                        <h3>{recipe.name}</h3>
+                        <p>Tags: {recipe.tags.map((tag, i) => { 
+                            return <span key={i}><a href="#">{tag}</a>&nbsp;</span>
+                            }) }
+                        </p>
+                        <p>
+                          <Button bsStyle="primary">See Recipe</Button>
+                        </p>
+                      </Thumbnail>
+                    </Col>
+            )
+        })
+     )
+  }
+
   render() {
     return (
         <div className="App">
@@ -23,27 +65,54 @@ class App extends Component {
             </div>
           </Router>
 
-          <div className="container home">
-            <Row className="show-grid">
-              <Col xs={12} md={8}>
-                <Jumbotron className="home__jumbotron">
-                  <h2>Welcome to Recipe App!</h2>
-                  <p>This app will hopefully do something some day so for now I'm just filling up space It also probably won't show when you are logged in...</p>
-                  <p>Login or Register to Get Started!</p>
-                  <p><Button bsStyle="primary">Take me there</Button></p>
-                </Jumbotron>
-              </Col>
-              <Col xs={12} md={4}>
-                <h2>Random Recipes</h2>
-                <ListGroup>
-                  <ListGroupItem href="#link1">Hiyashi Beef</ListGroupItem>
-                  <ListGroupItem href="#link2">Pesto Pasta</ListGroupItem>
-                  <ListGroupItem href="#link2">Okonomiyaki</ListGroupItem >
-                </ListGroup>
-              </Col>
-              </Row>
+          <div className="landing">
+            <div className="container home">
+              <Grid>
+                <Row className="">
+                  <Col xs={12} md={7}>
+                    <Jumbotron className="home__jumbotron">
+                      <h2>Welcome to Recipe App!</h2>
+                      <p>This app will hopefully do something some day so for now I'm just filling up space It also probably won't show when you are logged in...</p>
+                      <p>Login or Register to Get Started!</p>
+                      <p><Button bsStyle="primary">Take me there</Button></p>
+                    </Jumbotron>
+                  </Col>
+
+                  <Col xs={12} md={5}>
+                  <Jumbotron className="home__jumbotron">
+                    <h2>Random Recipes</h2>
+                    <Table hover={true}>
+                        <thead>
+                          <tr>
+                            <th></th>
+                            <th></th>
+                            <th>Author</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                        {this.getPublicRecipes()}
+                      </tbody>
+                    </Table>
+                    </Jumbotron>
+                  </Col>
+                </Row>
+              </Grid>
+            </div>
           </div>
-        </div>
+            
+          <div className="container home">
+              <Grid>
+              <Row className="show-grid">
+                <div>            
+                  <Col xs={12}>
+                  <h2><Glyphicon glyph="star" /> Featured Recipes</h2>
+                  </Col>
+                  {this.getFeaturedPublicRecipes()}
+                </div>
+              </Row>
+            </Grid>
+          </div>
+      </div>
     );
   }
 }
---
+++ src/sample_json.js
@@ -0,0 +1,84 @@
+let samplePublicRecipes = {
+    recipes: [
+        {
+            id: 1,
+            name: "Chicken Pesto Pasta",
+            featured: true,
+            images: {
+                thumbnail: 'http://images.media-allrecipes.com/userphotos/250x250/1316887.jpg'
+            },
+            tags: [
+                "italian", "chicken"
+            ],
+            author: "Michael F",
+            createdDate: "1491689117020"
+        },
+        {
+            id: 2,
+            name: "Slow Cooked Hayashi Rice",
+            featured: true,
+            images: {
+                thumbnail: 'http://www.justhungry.com/files/images/hayashi_raisu2.jpg'
+            },
+            tags: [
+                "japanese", "beef"
+            ],
+            author: "Michael F",
+            createdDate: "1491689117020"
+        },
+        {
+            id: 3,
+            name: "Okonomiyaki",
+            featured: true,
+            images: {
+                thumbnail: 'https://files.tofugu.com/articles/japan/2015-10-08-okonomiyaki/okonomiyaki-on-the-griddle.jpg'
+            },
+            tags: [
+                "japanese", "Pancake"
+            ],
+            author: "Michael F",
+            createdDate: "1491689117020"
+        },
+        {
+            id: 4,
+            name: "Sweet and Sticky Chicken Strips",
+            featured: true,
+            images: {
+                thumbnail: 'http://whiteonricecouple.com/recipe/images/sticky-chicken-tenders-recipe-2.jpg'
+            },
+            tags: [
+                "Chicken"
+            ],
+            author: "Michael F",
+            createdDate: "1491689117020"
+        },
+        {
+            id: 5,
+            name: "Pork Schnitzel",
+            featured: true,
+            images: {
+                thumbnail: 'http://assets.simplyrecipes.com/wp-content/uploads/2009/02/11174730/pork-schnitzel-horiz-b-1800.jpg'
+            },
+            tags: [
+                "Pork"
+            ],
+            author: "Michael F",
+            createdDate: "1491689117020"
+        },
+        {
+            id: 6,
+            name: "Mitarashi Dango",
+            featured: true,
+            images: {
+                thumbnail: 'http://www.japanesecooking101.com/wp-content/uploads/2016/11/IMG_9331.jpg'
+            },
+            tags: [
+                "Mochi", "Japanese", "Dessert"
+            ],
+            author: "Michael F",
+            createdDate: "1491689117020"
+        },
+    ]
+};
+
+export default samplePublicRecipes;
